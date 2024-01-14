b6672d17d58959bd9aa96ae49978ff77edca20ad
FuseSN2003 <sukavit.fuse2003@gmail.com>
2024-01-14 17:14:45 +0700

    update hw_5_4

--- hw_5/_4_sort_search_reduce.cpp
+++ hw_5/_4_sort_search_reduce.cpp
@@ -2,8 +2,92 @@
 
 using namespace std;
 
+int partition(int arr[], int l, int r) {
+  int pivot = arr[r];
+  int i = l - 1;
+
+  for(int j = l; j <= r - 1; j++) {
+    if(arr[j] < pivot) {
+      i++;
+      swap(arr[i], arr[j]);
+    }
+  }
+  i++;
+  swap(arr[i], arr[r]);
+
+  return i;
+}
+
+void quickSort(int arr[], int l, int r) {
+  if(l < r) {
+    int pivot = partition(arr, l, r);
+    
+    quickSort(arr, l, pivot - 1);
+    quickSort(arr, pivot + 1, r);
+  }
+}
+
 int main() {
+  int n;
+  cin >> n;
+
+  int arr[n];
+
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  quickSort(arr, 0, n - 1);
+
+  string s = "";
+
+  for(int i = 0; i < n; i++) {
+    s += to_string(arr[i]);
+  }
+  cout << s << endl;
   
-  
+  unordered_map<char, int> consecutiveMap;
+  for (int i = 0; i < s.length(); i++) {
+    int count = 1;
+
+    while (i < s.length() && s[i] == s[i + 1]) {
+      count++;
+      i++;
+    }
+    consecutiveMap[s[i]] = max(consecutiveMap[s[i]], count);
+  }
+
+  int maxConsecutive = INT_MIN;
+  for(const auto& pair : consecutiveMap) {
+    maxConsecutive = max(maxConsecutive, pair.second);
+  }
+
+  vector<int> tempMaxConsecutiveNums;
+  int maxSize = 0;
+  for(const auto &pair : consecutiveMap) {
+    if(pair.second == maxConsecutive) {
+      tempMaxConsecutiveNums.push_back(pair.first - '0');
+      maxSize++;
+    }
+  }
+
+  int maxConsecutiveNums[maxSize];
+  for(int i = 0; i < maxSize; i++) {
+    maxConsecutiveNums[i] = tempMaxConsecutiveNums[i];
+  }
+
+  quickSort(maxConsecutiveNums, 0, maxSize - 1);
+
+  for(int i = 0; i < maxSize; i++) {
+    cout << maxConsecutiveNums[i] << " ";
+  }
+  cout << endl;
+
+  for(int i = 0; i < s.length(); i++) {
+    if(s[i] != s[i+1]) {
+      cout << s[i];
+    }
+  }
+
   return 0;
 }
---
+++ other/maximum_sub_arr.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxSubarraySum(int *arr, int n) {
+  if(n == 1) return arr[0];
+
+  int m = n / 2;
+  int lmss = maxSubarraySum(arr, m);
+  int rmss = maxSubarraySum(arr+m, n-m);
+  int lsum = INT_MIN, rsum = INT_MIN, sum = 0;
+
+  for(int i = m; i < n; i++) {
+    sum += arr[i];
+    rsum = max(rsum, sum);
+  }
+
+  sum = 0;
+  for(int i = m - 1; i >= 0; i--) {
+    sum += arr[i];
+    lsum = max(lsum, sum);
+  }
+
+  return max(max(lmss, rmss), lsum + rsum);
+}
+
+int main() {
+  int maxNum = INT_MIN;
+  int n;
+  cin >> n;
+
+  int arr[n];
+
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  // for(int i = 0; i < n; i++) {
+  //   int sum = 0;
+  //   for(int j = i; j < n; j++){
+  //     sum += arr[j];
+  //     maxNum = max(maxNum, sum);
+  //   }
+  // }
+  
+  cout << maxSubarraySum(arr, n);
+  
+  return 0;
+}
